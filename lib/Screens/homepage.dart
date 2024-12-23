16e150076d077d886484456d051d60a5d4955422
youssef24413 <114123093+youssef24413@users.noreply.github.com>
2024-12-23 22:39:46 +0200

    Update homepage.dart

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'Drawer.dart'; // استيراد الـ Drawer
 import 'ProfileScreen.dart';
 import 'SearchScreen.dart';
 import 'SettingsScreen.dart';
@@ -26,115 +27,127 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_titles[_currentIndex]),
+        title: Text(
+          _titles[_currentIndex],
+          style: const TextStyle(fontSize: 20),
+        ),
         backgroundColor: Colors.green,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () {
-              // Navigate to the login page
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-            },
-          ),
-        ],
       ),
-
-      body: IndexedStack(
-        index: _currentIndex,
+      drawer: const AppDrawer(), // استخدام AppDrawer هنا
+      body: Stack(
         children: [
-          // Home Page
-          Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.home,
-                    size: 100,
-                    color: Colors.green,
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    'Welcome!',
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    'You have successfully logged in.',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      // Navigate to the PredictionScreen
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PredictionScreen(),
-                        ),
-                      );
-                    },
-                    icon: const Icon(Icons.camera_alt),
-                    label: const Text('Upload Photo for Prediction'),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 12,
-                      ),
-                      textStyle: const TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
+          // Background Image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/Untitled3 1.jpg'), // مسار الصورة
+                fit: BoxFit.cover,
               ),
             ),
           ),
-          SettingsScreen(),
-          ProfileScreen(),
-          SearchScreen(),
+          // Main Content
+          IndexedStack(
+            index: _currentIndex,
+            children: [
+              Center(
+                child: Container(), // الصفحة الرئيسية
+              ),
+              SettingsScreen(),
+              ProfileScreen(),
+              SearchScreen(),
+            ],
+          ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+      bottomNavigationBar: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.green, // إضافة اللون الأخضر للخلفية
+            selectedItemColor: Colors.white, // لون الأيقونات المختارة
+            unselectedItemColor: Colors.grey[300], // لون الأيقونات غير المختارة
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.green, // لون خلفية الأيقونة
+                    shape: BoxShape.circle, // جعل الخلفية دائرية
+                  ),
+                  padding: const EdgeInsets.all(10), // التحكم بحجم الدائرة
+                  child: const Icon(
+                    Icons.home,
+                    color: Colors.white, // لون الأيقونة داخل الخلفية
+                  ),
+                ),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: const Icon(
+                    Icons.settings,
+                    color: Colors.white,
+                  ),
+                ),
+                label: 'Settings',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+                label: 'Profile',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                ),
+                label: 'Search',
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
+          Positioned(
+            top: -30,
+            left: MediaQuery.of(context).size.width / 2 - 29,
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PredictionScreen()),
+                );
+              },
+              backgroundColor: Colors.green,
+              child: const Icon(Icons.camera_alt, size: 30),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+        ]
+        ,
       ),
     );
   }
